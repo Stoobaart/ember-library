5613e6d3038f5426034f691c5a413b3759d2e419
Stuart Smith <stuart.smith@ig.com>
2017-07-06 14:02:19 +0100

    added transition styles and new home page image

--- app/components/author-listing.js
+++ app/components/author-listing.js
@@ -1,18 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  editAuthor(author) {
-    author.set('isEditing', true);
-  },
-  cancelAuthorEdit(author) {
-    author.set('isEditing', false);
-    author.rollbackAttributes();
-  },
-  saveAuthor(author) {
-    if (author.get('isNotValid')) {
-      return;
+  actions: {
+    editAuthor(author) {
+      author.set('isEditing', true);
+    },
+    cancelAuthorEdit(author) {
+      author.set('isEditing', false);
+      author.rollbackAttributes();
+    },
+    saveAuthor(author) {
+      if (author.get('isNotValid')) {
+        return;
+      }
+      author.set('isEditing', false);
+      author.save();
     }
-    author.set('isEditing', false);
-    author.save();
   }
 });
